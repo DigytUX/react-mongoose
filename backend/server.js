9d78b4dd3403eb679e448fb66971e11c5668ce6f
Christopher <christopher@ChristophersMBP.attlocal.net>
2022-10-06 08:57:35 -0400

    Installed mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config({ path: "./config.env" });
 const express = require('express')
 const cors = require('cors')
 const app = express();
@@ -6,5 +6,8 @@ const PORT = process.env.PORT || 8080
 
 app.use(cors())
 app.use(express.json())
+app.use(require("./routes/record"));
 
-app.listen(PORT, ()=> console.log('server started'))
+app.listen(PORT, () => console.log('server started'))
+
+// https://www.mongodb.com/docs/atlas/cli/stable/install-atlas-cli/
