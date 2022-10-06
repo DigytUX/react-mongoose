63a678c591c5db6bacaf6484fa6fdbad2da827fc
Christopher <christopher@ChristophersMBP.attlocal.net>
2022-10-06 08:41:35 -0400

    Init commit

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express();
+const PORT = process.env.PORT || 8080
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(PORT, ()=> console.log('server started'))
